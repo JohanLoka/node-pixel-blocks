616060b0459c570476b379880a00f5be64f29971
Johan Lindberg <johanlindberg_1992@hotmail.se>
2018-01-01 21:54:54 +0100

    cors_added

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
 
-
 //Routes
 const players = require('./routes/players');
 const rounds = require('./routes/rounds');
@@ -14,10 +13,31 @@ const maps = require('./routes/maps');
 app.use(express.static(__dirname + '/public'));
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(cors());
+//app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/*
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  if ('OPTIONS' === req.method) {
+    res.send(204);
+  } else {
+    next();
+  }
+});
+*/
+
 //routes
 app.use('/players', players);
 app.use('/rounds', rounds);
@@ -27,7 +47,7 @@ app.use('/maps', maps);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
