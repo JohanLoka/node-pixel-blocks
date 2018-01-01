d75db2c56b569e5d8435cc93835b133e4581b7be
Johan Lindberg <johanlindberg_1992@hotmail.se>
2018-01-01 21:47:07 +0100

    cors_added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const app = express();
 
 
@@ -16,6 +17,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors());
 //routes
 app.use('/players', players);
 app.use('/rounds', rounds);
