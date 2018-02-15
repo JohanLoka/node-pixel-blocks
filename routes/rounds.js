97a8873e2f8f679000b42c14979e98cabbf0e458
Johan Lindberg <johanlindberg_1992@hotmail.se>
2018-02-15 21:38:20 +0100

    fixed daily reward backend

--- routes/rounds.js
+++ routes/rounds.js
@@ -8,14 +8,14 @@ const app = require('../app');
 const todayDate = () => {
   var dt = new Date();
   var month = dt.getMonth() + 1;
-  month = month >= 10 ?
-    month :
-    "0" + month;
+  month = month >= 10
+    ? month
+    : "0" + month;
 
   var day = dt.getDate();
-  day = day >= 10 ?
-    day :
-    "0" + day;
+  day = day >= 10
+    ? day
+    : "0" + day;
   var date = dt.getFullYear() + "-" + month + "-" + day;
   return date;
 };
@@ -23,14 +23,14 @@ const todayDate = () => {
 const formatDate = (input) => {
   var dt = input;
   var month = dt.getMonth() + 1;
-  month = month >= 10 ?
-    month :
-    "0" + month;
+  month = month >= 10
+    ? month
+    : "0" + month;
 
   var day = dt.getDate();
-  day = day >= 10 ?
-    day :
-    "0" + day;
+  day = day >= 10
+    ? day
+    : "0" + day;
   var date = dt.getFullYear() + "-" + month + "-" + day;
   return date;
 };
@@ -59,8 +59,6 @@ router.get('/todays', (req, res) => {
   });
 });
 
-
-
 //Top players all time
 router.get('/toplist/today', (req, res) => {
   var date = todayDate();
@@ -73,33 +71,37 @@ router.get('/toplist/today', (req, res) => {
 
 //Top players all time
 router.get('/toplist', (req, res) => {
-    var sql = `SELECT MAX(score) AS score, players.username AS username, SUBSTRING(rounds.date,1,10) AS date FROM rounds INNER JOIN players ON players.id=rounds.player_id WHERE ranked='True' GROUP BY username ORDER BY score DESC LIMIT 5`;
-    pool.query(sql, function(err, result, fields) {
-          res.send(result);
-    });
+  var sql = `SELECT MAX(score) AS score, players.username AS username, SUBSTRING(rounds.date,1,10) AS date FROM rounds INNER JOIN players ON players.id=rounds.player_id WHERE ranked='True' GROUP BY username ORDER BY score DESC LIMIT 5`;
+  pool.query(sql, function(err, result, fields) {
+    res.send(result);
+  });
 });
-
-
 
 //Players best today
 router.get('/todays/:id', (req, res) => {
   const date = todayDate();
 
-  var sql = `SELECT score, players.username AS username FROM rounds INNER JOIN players ON players.id=rounds.player_id WHERE date='${date}' AND ranked='True' AND rounds.player_id=${req.params.id} ORDER BY score DESC LIMIT 1`;
+  var sql = `SELECT score, players.username AS username FROM rounds INNER JOIN players ON players.id=rounds.player_id WHERE date='${date}' AND ranked='True' AND rounds.player_id=${req.params.id} ORDER BY score`;
   pool.query(sql, function(err, result, fields) {
     if (err)
       throw err;
 
     var items = [];
     var arr = [];
-    result.forEach(function(row) {
+    if (result.length > 0) {
       var newarr = {
-        username: row.username,
-        score: row.score
-      }
-      arr.push(newarr);
-    });
-
+        username: result[0].username,
+        score: result[0].score,
+        count: result.length
+      };
+    } else {
+      var newarr = {
+        username: "NO_GAMES",
+        score: 0,
+        count: 0
+      };
+    }
+    arr.push(newarr);
     items['items'] = arr;
     res.send(arr[0]);
   });
