0aef5d57701d1979227095ccc59b4f962ff5b575
Johan Lindberg <johanlindberg_1992@hotmail.se>
2018-08-28 09:37:28 +0200

    Update maps.js

--- routes/maps.js
+++ routes/maps.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('../db');
 const router = express.Router();
-const formatDate = require('../lib/date');
+const { today } = require('../lib/date');
 
 
 //Update settings
